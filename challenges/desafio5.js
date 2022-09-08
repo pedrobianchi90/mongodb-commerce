3ce8b9bd28efdce0e939c9253ec3ccfea53ead37
Pedro Bianchi <pedroedm@gmail.com>
2022-09-08 17:37:38 -0300

    Realiza requisito 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -4,9 +4,9 @@
 // igual a 85
 
 db.produtos.find(
-    {$or: [
-       { curtidas: { $eq: 36 }},
-       { vendidos: { $eq: 85 }},
-    ]},
+    { $or: [
+       { curtidas: { $eq: 36 } },
+       { vendidos: { $eq: 85 } },
+    ] },
     { _id: 0, nome: 1, curtidas: 1, vendidos: 1 },
 );
