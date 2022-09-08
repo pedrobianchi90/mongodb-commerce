a07e3451ccd985fdb954285e6bfdd850310ba30a
Pedro Bianchi <pedroedm@gmail.com>
2022-09-08 16:37:58 -0300

    Realiza desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,10 @@
+// Retorne o lanche mais vendido, 
+// mostrando apenas o nome e a quantidade
+//  do lanche mais vendido
+
+db.produtos.find(
+    {},
+    { _id: 0, nome: 1, vendidos: 1 }
+).sort(
+    { vendidos: -1 }
+).limit(1);
