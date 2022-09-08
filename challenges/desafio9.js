95935852c8fad85c08bb587cd90be1a1cc7969fc
Pedro Bianchi <pedroedm@gmail.com>
2022-09-08 18:49:32 -0300

    Realiza desafio 10

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,6 +1,3 @@
-// Retorne o nome de todos os lanches que possuam 
-// calorias abaixo de 500
-
 db.produtos.find(
     {
         valoresNutricionais: {
@@ -8,9 +5,9 @@ db.produtos.find(
                 "tipo": "calorias",
                 "quantidade": {
                     $lt: 500
-                }
-            }
-        }
+                },
+            },
+        },
     },
     { _id: 0, nome: 1 },
 );
